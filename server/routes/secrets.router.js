9a007789635cc40fb321af3beed1affb1e5e3586
Nick Neiert <nickster7421@gmail.com>
2019-02-06 13:55:16 -0600

    completed base

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -3,13 +3,14 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    if (req.isAuthenticated()){
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
-        .then(results => res.send(results.rows))
-        .catch(error => {
-            console.log('Error making SELECT for secrets:', error);
-            res.sendStatus(500);
-        });
+    pool.query(`SELECT * FROM "secret" WHERE${req.user.clearance_level >= 10};`);
+ }
+ .then(results => res.send(results.rows)){
+        res.sendStatus(403);
+    }
+ 
 });
 
 module.exports = router;
